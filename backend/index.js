761385b7bc4e39fdd786806fc35a66d4fc9d12e3
4twqrdq5jz-ui <4twqrdq5jz@privaterelay.appleid.com>
2026-02-04 22:28:52 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -382,6 +382,6 @@ app.post("/shipping/:shopId/:orderId", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
+app.listen(PORT, "0.0.0.0", () =>
   console.log(`Printify backend running on port ${PORT}`)
 );
